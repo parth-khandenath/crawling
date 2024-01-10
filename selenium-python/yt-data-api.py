804a3e70f33fb3d0bbde004a41d03625006d3a21
parth-khandenath <parth.khandenath@pocketfm.com>
2024-01-10 23:52:21 +0530

    apikey quota exceeded handled

--- selenium-python/yt-data-api.py
+++ selenium-python/yt-data-api.py
@@ -1,17 +1,21 @@
-import time
 import pandas as pd
-import urllib3
+import os
 # import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
 import googleapiclient.errors
+from dotenv import load_dotenv
 
-channel_id='UCQatgKoA7lylp_UzvsLCgcw'
-apikey_lsts=['AIzaSyAN6JpbgUwqCl1iScIkZxkRrQBYB6zvpd0','AIzaSyDeo9BDSmJcvMLD2oVDlIvF7ONYTbu84nY','AIzaSyD3dhOZFfNt8yXqbUfsINaQ0QrSMlC4-g0','AIzaSyCTYE4tkcLMU1nih0vxvr8uXz_WBJFLfHg','AIzaSyCoRImQPn5kEX1rynV6HUawv5Lropgv1zc','AIzaSyCqqo5Rtfj9_UxJE6uunFCA1bx6DJWIacw','AIzaSyCNgIwxiAh_XF7tmzXx5h9jYD9Ywd-dBDE','AIzaSyAFloRMgEuSodkaIjOwz5u0CmWPAmSbaTU','AIzaSyCGQydk53JSglFM4QmwmhWY4d5faZ0ni2s','AIzaSyB6GEfL2pGdhA1D_1j2yAs77pmRj0iq2eA']
-ptr=0
-api_key=apikey_lsts[ptr]
+load_dotenv()
+
+channel_id='UCQatgKoA7lylp_UzvsLCgcw'   #change here
+file_name='yt-trial.csv'    #change here
+# apikey_lsts=['AIzaSyAN6JpbgUwqCl1iScIkZxkRrQBYB6zvpd0','AIzaSyDeo9BDSmJcvMLD2oVDlIvF7ONYTbu84nY','AIzaSyD3dhOZFfNt8yXqbUfsINaQ0QrSMlC4-g0','AIzaSyCTYE4tkcLMU1nih0vxvr8uXz_WBJFLfHg','AIzaSyCoRImQPn5kEX1rynV6HUawv5Lropgv1zc','AIzaSyCqqo5Rtfj9_UxJE6uunFCA1bx6DJWIacw','AIzaSyCNgIwxiAh_XF7tmzXx5h9jYD9Ywd-dBDE','AIzaSyAFloRMgEuSodkaIjOwz5u0CmWPAmSbaTU','AIzaSyCGQydk53JSglFM4QmwmhWY4d5faZ0ni2s','AIzaSyB6GEfL2pGdhA1D_1j2yAs77pmRj0iq2eA']
+total_api_keys=int(os.getenv('total_api_keys'))
+ptr=1
+api_key= os.getenv(f'youtube_data_apikey{ptr}') #apikey_lsts[ptr]
 youtube =  build('youtube', 'v3', developerKey=api_key)
-playlist_ids=[]
-playlist_ids=['UUQatgKoA7lylp_UzvsLCgcw']
+# playlist_ids=[]
+playlist_ids=[channel_id]  #a channel is also a playlist
 # next_page_token=None
 # request_no=0
 # while True:
@@ -46,41 +50,37 @@ df_header={
         'views':[],
         'date':[]
     }
-file_name='yt-trial.csv'
 try:
-    pd.read_csv(file_name)
+    df=pd.read_csv(file_name)
 except:
     df=pd.DataFrame(df_header)
 
 next_page_token=None
 video_count=0
-# my_file=open("pagefile.txt",'w+')
-# next_page_token=my_file.readline()
-# print('starting from page token:',next_page_token)
 for pl_id in playlist_ids:
     while True:
-        my_file=open("pagefile.txt",'w+')
-        next_page_token=my_file.readline().strip()
-        # my_file.write(str(next_page_token))
+        my_file=open("C:\\Users\\Admin\\Desktop\\pfm\\crawling\\pagefile.txt",'r')
+        next_page_token=my_file.read().strip()
         my_file.close()
-        try:
-            if next_page_token=="None":
-                next_page_token=None
-            print('next page:',next_page_token)
-            request = youtube.playlistItems().list(part="id,snippet,status,contentDetails",maxResults=50,pageToken=next_page_token,playlistId=pl_id)
-            response = request.execute()
-        except googleapiclient.errors.HttpError: #quota exceeded
-            # my_file.close()
-            print('switching api key')
-            ptr+=1
-            if len(apikey_lsts)==ptr:
-                print("no more api keys...")
-            api_key=apikey_lsts[ptr]
-            youtube =  build('youtube', 'v3', developerKey=api_key)
-            request = youtube.playlistItems().list(part="id,snippet,status,contentDetails",maxResults=50,pageToken=next_page_token,playlistId=pl_id)
-            response = request.execute()
-        print("##################")
-        print(len(response['items']))
+        if next_page_token=="None":
+            next_page_token=None
+        print('next page:',next_page_token)
+        while True:
+            try:    
+                youtube =  build('youtube', 'v3', developerKey=api_key)
+                request = youtube.playlistItems().list(part="id,snippet,status,contentDetails",maxResults=50,pageToken=next_page_token,playlistId=pl_id)
+                response = request.execute()
+                break
+            except googleapiclient.errors.HttpError: #quota exceeded
+                print('switching api key')
+                ptr+=1
+                if ptr>total_api_keys:
+                    print("no more api keys...")
+                    break
+                else:
+                    api_key=os.getenv(f'youtube_data_apikey{ptr}')
+        # print("##################")
+        # print(len(response['items']))
         for plitem in response['items']:
             if plitem['status']['privacyStatus']=='private': #private video, skip
                 continue
@@ -89,36 +89,36 @@ for pl_id in playlist_ids:
             video_title=(plitem['snippet']['title'])
             video_date=(plitem['snippet']['publishedAt'])
             # video_desc=(plitem['snippet']['description'])
-            try:
-                request2 = youtube.videos().list(part="statistics",id=video_id)
-                response2 = request2.execute()
-            except googleapiclient.errors.HttpError: #quota exceeded
-                print('switching api key')
-                ptr+=1
-                if len(apikey_lsts)==ptr:
-                    print("no more api keys...")
-                api_key=apikey_lsts[ptr]
-                youtube =  build('youtube', 'v3', developerKey=api_key)
-                request2 = youtube.videos().list(part="statistics",id=video_id)
-                response2 = request2.execute()
-            # except:
-            #     my_file.close()
+            while True:
+                try:
+                    youtube =  build('youtube', 'v3', developerKey=api_key)
+                    request2 = youtube.videos().list(part="statistics",id=video_id)
+                    response2 = request2.execute()
+                    break
+                except googleapiclient.errors.HttpError: #quota exceeded
+                    print('switching api key')
+                    ptr+=1
+                    if ptr>total_api_keys:
+                        print("no more api keys...")
+                        break
+                    else:
+                        api_key=os.getenv(f'youtube_data_apikey{ptr}')
             try:
                 views=response2['items'][0]['statistics']['viewCount']  #likes,comments,favourites are also fetchable 
                 # if int(views)>100000:  #less views, skip
                 df.loc[len(df.index)]=[video_title,views,video_date]
                 df.to_csv(file_name,index=False)    
             except Exception as e:
-                # my_file.close()
                 print(video_id,':',e)
         try:
             next_page_token=response['nextPageToken']
-        except:
+        except Exception as e:
+            print('err:',e)
+            print(response)
             print("BREAKING......1")
             break
-            # next_page_token=None
         print("videos done:",video_count)
-        my_file=open("pagefile.txt",'w+')
+        my_file=open("C:\\Users\\Admin\\Desktop\\pfm\\crawling\\pagefile.txt",'w')
         my_file.write(str(next_page_token))
         my_file.close()
         if next_page_token==None or len(next_page_token)==0:
